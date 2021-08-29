5f547fb71086aeb01b46c63b5351213ce3c8ee73
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2021-08-29 03:45:07 -0300

    criando interface de validador

--- src/usuarios/validadores/email/email-valido.ts
+++ src/usuarios/validadores/email/email-valido.ts
@@ -1,7 +1,8 @@
 import * as EmailValidator from 'email-validator';
+import { Validador } from '../../../validadores/validador';
 
 
-export class EmailValido {
+export class EmailValido implements Validador {
 
   valido(email: string): boolean {
     return email !== undefined &&
---
+++ src/validadores/validador.ts
@@ -0,0 +1,3 @@
+export type Validador = {
+  valido(item): boolean
+}
